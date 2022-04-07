a4666053d1c963dcaca754e3ef0f75903694fbc5
David Bokil <david.bokil@David-Bokil-MBP.fios-router.home>
2022-04-07 00:55:37 -0400

    media query to ensure text doesnt bleed on vertically challenged screens

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-newhome-js": () => import("./../../../src/pages/newhome.js" /* webpackChunkName: "component---src-pages-newhome-js" */)
 }
 
---
+++ src/pages/newhome.js
@@ -0,0 +1,26 @@
+import React from "react"
+import './index.css'
+import { GatsbySeo } from 'gatsby-plugin-next-seo';
+// import { StaticQuery, graphql } from 'gatsby'
+
+export default function Home() {
+    return <main>	
+		  <GatsbySeo
+		      title='Dave Bokil - Web Developer'
+		      description='Dave Bokil is a powerful full stack javascript developer with the innate gift of spotting potential optimizations in UX design and implementing improvements at a rapid pace.'
+		    />
+			<div className="frame">
+				<div className="frame__links">
+					<a href="https://www.linkedin.com/in/davebokil/" target="_blank" rel="noreferrer">LinkedIn</a>
+					<a href="https://github.com/davebokil" target="_blank" rel="noreferrer">GitHub</a>
+					<a href="mailto:bokild@gmail.com">Contact</a>
+				</div>
+			</div>
+			<div className="content">
+				<h2 className="content__title"><span>Dave Bokil</span><em>Web Developer</em></h2>
+				<p>Dave Bokil is a powerful Full Stack Javascript Developer with the innate gift of spotting potential optimizations in UX design and implementing solutions and improvements at a rapid pace.</p>
+				<p>He is currently Senior Full Stack Developer at the <a href="https://www.nbpa.com/" target="blank">National Basketball Player's Association</a></p>
+				<p>© 2022</p>
+			</div>
+		</main>
+}
